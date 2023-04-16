4130aeb7b8188c956fd7aa9732c553406aa97e5a
ThuanNQT <thuan13112003@gmail.com>
2023-04-16 08:05:11 +0700

    update by NQT

---
+++ GameKeoBuaBao/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace GameKeoBuaBao
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.picPlayer = new System.Windows.Forms.PictureBox();
+            this.picCPU = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblCountDown = new System.Windows.Forms.Label();
+            this.lblRounds = new System.Windows.Forms.Label();
+            this.countDownTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCPU)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRock
+            // 
+            this.btnRock.Location = new System.Drawing.Point(56, 47);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(94, 29);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Location = new System.Drawing.Point(56, 106);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(94, 29);
+            this.btnPaper.TabIndex = 0;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.Location = new System.Drawing.Point(56, 166);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(94, 29);
+            this.btnScissors.TabIndex = 0;
+            this.btnScissors.Text = "Scissors";
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(677, 409);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(94, 29);
+            this.btnRestart.TabIndex = 0;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // picPlayer
+            // 
+            this.picPlayer.Image = global::GameKeoBuaBao.Properties.Resources.qq;
+            this.picPlayer.Location = new System.Drawing.Point(200, 47);
+            this.picPlayer.Name = "picPlayer";
+            this.picPlayer.Size = new System.Drawing.Size(154, 148);
+            this.picPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlayer.TabIndex = 1;
+            this.picPlayer.TabStop = false;
+            // 
+            // picCPU
+            // 
+            this.picCPU.Image = global::GameKeoBuaBao.Properties.Resources.qq;
+            this.picCPU.Location = new System.Drawing.Point(594, 47);
+            this.picCPU.Name = "picCPU";
+            this.picCPU.Size = new System.Drawing.Size(154, 148);
+            this.picCPU.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCPU.TabIndex = 1;
+            this.picCPU.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Player";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(648, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPU";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblScore.Location = new System.Drawing.Point(389, 42);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(178, 30);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Player 0 : CPU 0";
+            // 
+            // lblCountDown
+            // 
+            this.lblCountDown.AutoSize = true;
+            this.lblCountDown.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCountDown.Location = new System.Drawing.Point(451, 106);
+            this.lblCountDown.Name = "lblCountDown";
+            this.lblCountDown.Size = new System.Drawing.Size(26, 30);
+            this.lblCountDown.TabIndex = 2;
+            this.lblCountDown.Text = "5";
+            // 
+            // lblRounds
+            // 
+            this.lblRounds.AutoSize = true;
+            this.lblRounds.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblRounds.Location = new System.Drawing.Point(411, 178);
+            this.lblRounds.Name = "lblRounds";
+            this.lblRounds.Size = new System.Drawing.Size(104, 30);
+            this.lblRounds.TabIndex = 2;
+            this.lblRounds.Text = "Round: 3";
+            // 
+            // countDownTimer
+            // 
+            this.countDownTimer.Interval = 1000;
+            this.countDownTimer.Tick += new System.EventHandler(this.countDownTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRounds);
+            this.Controls.Add(this.lblCountDown);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picCPU);
+            this.Controls.Add(this.picPlayer);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCPU)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnRock;
+        private Button btnPaper;
+        private Button btnScissors;
+        private Button btnRestart;
+        private PictureBox picPlayer;
+        private PictureBox picCPU;
+        private Label label1;
+        private Label label2;
+        private Label lblScore;
+        private Label lblCountDown;
+        private Label lblRounds;
+        private System.Windows.Forms.Timer countDownTimer;
+    }
+}
---
+++ GameKeoBuaBao/Form1.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System.Diagnostics.Contracts;
+
+namespace GameKeoBuaBao
+{
+    public partial class Form1 : Form
+    {
+        int rounds =3;//lượt chơi 
+        int timerPerRound = 6;//thời gian chạy 
+        bool gameOver = false;//check xem hết 1 ván game chưa 
+
+        //tạo Random cho CPU chọn
+        string[] CPUchoiceList = { "rock", "paper", "scrissors", "rock", "paper", "scrissors" };
+        int randomNumber = 0;//chỉ số của mảng
+        Random rnd = new Random();
+
+        //lựa chọn của CPU và Player
+        string CPUChoice;
+        string PlayerChoice;
+
+        //Điểm của Player và CPU
+        int PlayerScore;
+        int CPUScore;
+
+        public Form1()
+        {
+            InitializeComponent();
+            countDownTimer.Enabled= true;//thời gian bắt đầu chạy
+            PlayerChoice = "none";//người chơi không chọn gì
+            lblCountDown.Text = "5";
+
+        }
+
+        //Xử lý các lựa chọn của người chơi
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.rock;
+            PlayerChoice= "rock";
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.paper;
+            PlayerChoice= "paper";
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.scissors;
+            PlayerChoice= "scrissors";
+        }
+
+
+        //làm mới lại game
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            //trả lại giá trị ban đầu
+            PlayerScore= 0;
+            CPUScore= 0;
+            rounds = 3;
+
+            lblScore.Text = "Player: " + PlayerScore + "  CPU: " + CPUScore;
+
+            countDownTimer.Enabled = true;//bắt đầu lại bộ đếm thời gian
+
+            //trả lại hình ảnh ban đầu 
+            picPlayer.Image = Properties.Resources.qq;
+            picCPU.Image = Properties.Resources.qq;
+
+            gameOver = false;//đánh dấu chưa kết thúc game
+        }
+
+
+
+        private void countDownTimer_Tick(object sender, EventArgs e)
+        {
+            //Thời gian giảm dần
+            timerPerRound -= 1;
+            lblCountDown.Text=timerPerRound.ToString();
+
+            //Lượt chơi
+            lblRounds.Text = "Rounds: " + rounds;
+
+            //Nếu thời gian chạy về 0
+            if (timerPerRound < 1)
+            {
+                countDownTimer.Enabled = false;//dừng thời gian
+                timerPerRound= 6;//Trả lại giá trị để bắt đầu lượt chơi tiếp theo
+
+                //Xử lý lựa chọn cho CPU
+                randomNumber= rnd.Next(0,CPUchoiceList.Length);
+                CPUChoice = CPUchoiceList[randomNumber];
+
+                switch(CPUChoice)
+                {
+                    case "rock":
+                        picCPU.Image = Properties.Resources.rock;
+                        break;
+                    case "paper":
+                        picCPU.Image = Properties.Resources.paper;
+                        break;
+                    case "scrissors":
+                        picCPU.Image = Properties.Resources.scissors;
+                        break;
+                }
+
+                //Xử lý từng lượt chơi đến khi kết thúc 1 ván
+                if (rounds > 0)
+                {
+                    checkGame();
+                }
+                else
+                {
+                    if (PlayerScore > CPUScore)
+                    {
+                        MessageBox.Show("Player won the game !");
+                    }
+                    else
+                    {
+                        MessageBox.Show("CPU won the game !");
+                    }
+                    gameOver = true;//đãkết thúc 1 ván game
+                }
+            }           
+        }
+
+
+        //Xử lý thắng - thua trong Game
+        private void checkGame()
+        {
+            if (PlayerChoice == "rock" && CPUChoice == "paper")
+            {
+                CPUScore++;//tăng điểm
+                rounds--;//Giảm số lượng lượt chơi
+                MessageBox.Show("CPU Win");
+            }
+            else if (PlayerChoice == "scrissors" && CPUChoice == "rock")
+            {
+                CPUScore++;
+                rounds--;
+                MessageBox.Show("CPU Win");
+            }
+            else if (PlayerChoice == "paper" && CPUChoice == "scrissors")
+            {
+                CPUScore++;
+                rounds--;
+                MessageBox.Show("CPU Win");
+            }
+            else if (PlayerChoice == "rock" && CPUChoice == "scrissors")
+            {
+                PlayerScore++;
+                rounds--;
+                MessageBox.Show("Player Win");
+            }
+            else if (PlayerChoice == "paper" && CPUChoice == "rock")
+            {
+                PlayerScore++;
+                rounds--;
+                MessageBox.Show("Player Win");
+            }
+            else if (PlayerChoice == "scrissors" && CPUChoice == "paper")
+            {
+                PlayerScore++;
+                rounds--;
+                MessageBox.Show("Player Win");
+            }
+            else if (PlayerChoice == "none") //người chơi không chọn gì
+            {
+                MessageBox.Show("Make a choice");
+
+            }
+            else { MessageBox.Show("Equals !!!"); }// Các trường hợp bằng nhau
+
+            startNextRound();//Bắt đầu ván mới
+        }
+
+        //Xử lý ván chơi tiếp theo
+        private void startNextRound()
+        {
+            if (gameOver==true)//nếu đã kết thúc game
+            {
+                return;
+            }
+
+            //Nếu chưa hết 1 ván chơi
+            lblScore.Text = "Player: " + PlayerScore + "  CPU: " + CPUScore;//cập nhật điểm số của Player và CPU
+
+            countDownTimer.Enabled = true;//resert lại bộ đếm thời gian
+
+            //trả lại hình ảnh ban đầu
+            picPlayer.Image = Properties.Resources.qq;
+            picCPU.Image = Properties.Resources.qq;
+        }
+
+    }
+}
